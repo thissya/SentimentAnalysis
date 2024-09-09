2756aa83b159559acb7a02fa45a675258aff9c53
Thissyakkanna <thissyakkannasm.22aid@kongu.edu>
2024-09-09 18:44:46 +0530

    update requirements

--- Backend/backend.py
+++ Backend/backend.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 
-with open('E:/BDA Mini/Backend/ml model/logistic_regression_model.pkl', 'rb') as model_file:
+with open('E:\BDA Mini\Backend\logistic_regression_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
     print("\nModel loaded successfully:", model)
 
-with open('E:/BDA Mini/Backend/ml model/tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+with open('E:\BDA Mini\Backend\tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
     print("\n\nVectorizer loaded successfully:", vectorizer)
 
