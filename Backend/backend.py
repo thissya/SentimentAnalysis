10586a5298f4099b746a7b235f19d56f47535aab
thissya <thissya129@gmail.com>
2024-11-12 11:01:50 +0530

    previous commits

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, jsonify
-import pickle
+from flask import Flask ,request,jsonify
+import pickle 
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-with open('text_classification_pipeline.pkl', 'rb') as pipeline_file:
-    pipeline = pickle.load(pipeline_file)
-    print("\nPipeline loaded successfully:", pipeline)
+
+with open('logistic_regression_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+    print("\nModel loaded successfully:", model)
+
+with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+    vectorizer = pickle.load(vectorizer_file)
+    print("\n\nVectorizer loaded successfully:", vectorizer)
+
+print("\n\n")
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -18,10 +25,11 @@ def predict():
     
     print("\nReceived input data:", input_data)
     
-    prediction = pipeline.predict([input_data])
+    transformed_input = vectorizer.transform([input_data])
+    prediction = model.predict(transformed_input)
     prediction_value = prediction[0]
-    
-    print("Prediction:", prediction_value)
+    print(prediction_value)
+    print("\n")
     return jsonify({'prediction': prediction_value})
 
 if __name__ == '__main__':
